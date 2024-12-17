48092eb686d7e8e575c2cf07993255718a15555e
filiptota <filip.tota5@gmail.com>
2024-12-17 22:00:15 +0100

    node crash course setup

---
+++ index.js
@@ -0,0 +1,10 @@
+// const { generateRandomNumber, celsiusToFahrenheit } = require("./utils");
+
+// console.log("hello world");
+// console.log("generateRandomNumber() :>> ", generateRandomNumber());
+// console.log("celsiusToFahrenheit(0) :>> ", celsiusToFahrenheit(0));
+
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log("getPosts() :>> ", getPosts());
+console.log("getPostsLength() :>> ", getPostsLength());
